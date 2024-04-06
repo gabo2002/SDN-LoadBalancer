5ecc77a7e8646ea31470a109302e49e351544513
Gabo <gabriele.corti2002@gmail.com>
2024-04-06 15:07:47 +0200

    added json redundant validation added mininet network cleanup

--- network_controller/main.py
+++ network_controller/main.py
@@ -1,16 +1,18 @@
 from network_topology import NetworkTopology
 from mininet.net import Mininet
 from mininet.log import setLogLevel
-
+from mininet.clean import Cleanup
 
 def main():
     #Create a network topology object
     topology = NetworkTopology('../config/switches.json')
     
     #Create a Mininet object
-    net = Mininet(topology,controller=None,autoSetMacs=True,autoStaticArp=False)
+    network = Mininet(topology,controller=None,autoSetMacs=True,autoStaticArp=False)
 
 
 if __name__ == "__main__":
-    setLogLevel('debug')
+    setLogLevel('info')
+    #remove any existing Mininet instances
+    Cleanup.cleanup()
     main()
--- network_controller/network_topology.py
+++ network_controller/network_topology.py
@@ -26,6 +26,8 @@ class NetworkTopology(Topo):
         self.json_path = json_path
         Topo.__init__(self)
 
+    # Build the network topology by initializing the switches, hosts, and links 
+    # This method is called by the Mininet object when it is created
     def build(self):
         try:
             self.init_topology()
@@ -35,6 +37,7 @@ class NetworkTopology(Topo):
             print("Exiting...")
             exit(1)
 
+    # Initialize the network topology by reading the JSON file and creating the switches, hosts, and links
     def init_topology(self):
         json_data = self.load_json()
 
@@ -68,7 +71,7 @@ class NetworkTopology(Topo):
                 print("Connecting switch {} to switch {}".format(switch_id, connected_switch_id))
                 self.addLink(switch_id, connected_switch_id)
 
-    
+    # Load the JSON network topology file and validate it
     def load_json(self):
         json_file = open(self.json_path, 'r')
         json_data = json.load(json_file)
@@ -78,10 +81,28 @@ class NetworkTopology(Topo):
             print("Invalid JSON file: 'switches' key not found or extra keys found")
             raise Exception('Invalid JSON file')
         
-        for switch in json_data['switches']:
+        for switch in json_data['switches']:    #check if all keys are valid
             keys = switch.keys()
             for key in keys:
                 if key not in ['id', 'hosts', 'connected_switches']:
                     raise Exception('Invalid JSON file')
+
+        #Unique switch ids
+        switch_ids = set()
+        for switch in json_data['switches']:
+            if switch['id'] in switch_ids:
+                print("The switch with id {} is not unique".format(switch['id']))
+                raise Exception('Switch id not unique')
+            switch_ids.add(switch['id'])
+
+        #redundant switch connections
+        links = set()
+        for switch in json_data['switches']:
+            for connected_switch in switch['connected_switches']:
+                link = (switch['id'], connected_switch)
+                if link in links or (link[1], link[0]) in links:
+                    print("The link between switch {} and switch {} is redundant".format(link[0], link[1]))
+                    raise Exception('Redundant switch connections')
+                links.add(link)
 
         return json_data
